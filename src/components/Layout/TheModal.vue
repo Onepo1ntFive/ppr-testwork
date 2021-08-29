96061e4a66596d72c81594f2d018e2002e04f844
SergeySolovev <name.diesel@gmail.com>
2021-08-29 19:59:37 +0300

    Modal for carousel images

---
+++ src/components/Layout/TheModal.vue
@@ -0,0 +1,85 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-dialog">
+          <button type="button" class="close" @click="closeModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <img :src="imagePath" alt="imagePath" />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ["imagePath"],
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  &-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    justify-content: center;
+    transition: opacity 0.3s ease;
+  }
+  &-dialog {
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    max-width: none;
+    pointer-events: all;
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+  &-wrapper {
+    width: calc(100% - 2rem);
+    height: calc(100% - 2rem);
+  }
+}
+.close {
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  z-index: 9999;
+  opacity: 1;
+  font-size: 3rem;
+  font-weight: 300;
+  line-height: 0.3;
+  color: #fff;
+
+  &:not(:disabled):not(.disabled):hover,
+  &:not(:disabled):not(.disabled):focus {
+    opacity: 1;
+  }
+
+  &:hover {
+    color: #fff;
+    opacity: 0.75;
+  }
+}
+</style>
--- src/components/Movies/MoviesImages.vue
+++ src/components/Movies/MoviesImages.vue
@@ -3,9 +3,11 @@
     <h2>Images</h2>
     <carousel :settings="settings" :breakpoints="breakpoints">
       <slide v-for="image in images" :key="image.file_path">
-        <a
-          :href="
-            imagesConfig.base_url + imagesConfig.full_size + image.file_path
+        <button
+          @click="
+            showModal(
+              imagesConfig.base_url + imagesConfig.full_size + image.file_path
+            )
           "
         >
           <img
@@ -14,27 +16,38 @@
             "
             :alt="`${alt}-${image.file_path}`"
           />
-        </a>
+        </button>
       </slide>
       <template #addons>
         <navigation />
       </template>
     </carousel>
   </div>
+
+  <the-modal
+    @close-modal="closeModal"
+    :imagePath="imagePath"
+    v-if="modalCanShow"
+  ></the-modal>
 </template>
 
 <script>
-import "vue3-carousel/dist/carousel.css";
+import TheModal from "@/components/Layout/TheModal";
 import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 export default {
   components: {
     Carousel,
     Slide,
     Pagination,
     Navigation,
+    TheModal,
   },
   data() {
     return {
+      modalCanShow: false,
+      imagePath: "",
+      // carousel settings
       settings: {
         itemsToShow: 1,
         wrapAround: true,
@@ -60,6 +73,16 @@ export default {
       return this.$store.getters.getImagesConf;
     },
   },
+  methods: {
+    showModal(path) {
+      this.modalCanShow = true;
+      this.imagePath = path;
+    },
+    closeModal() {
+      this.modalCanShow = false;
+      this.imagePath = '';
+    },
+  },
   mounted() {
     this.$store.dispatch("getImages", { movieId: this.movieId });
     this.$store.dispatch("getImagesConf");
@@ -69,15 +92,20 @@ export default {
 
 <style lang="scss" scoped>
 .carousel {
-  margin-left: -0.5rem;
-  margin-right: -0.5em;
-  img {
-    max-width: 100%;
-  }
-
+  
   &__slide {
     padding-left: 0.5rem;
     padding-right: 0.5rem;
+
+    img {
+      max-width: 100%;
+    }
+    button {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      background: none;
+    }
   }
 }
 </style>
